c2db0d4c853bedb4006ec9a39d26027e0607e0f4
Sneha Santhosh <45783979+Sneha-Santhosh@users.noreply.github.com>
2020-11-17 22:03:04 -0500

    Code cleanup

--- Models/ClubRepository.cs
+++ Models/ClubRepository.cs
@@ -5,22 +5,20 @@ using System.Threading.Tasks;
 
 namespace Sneha_S_301096645.Models
 {
-    public static class ClubRepository
+  public static class ClubRepository
     {
-       
-            private static List<ClubResponse> clubResponses = new List<ClubResponse>();
-            public static IEnumerable<ClubResponse> ClubResponse
+        private static List<ClubResponse> clubResponses = new List<ClubResponse>();
+        public static IEnumerable<ClubResponse> ClubResponse
+        {
+            get
             {
-                get
-                {
-                    return clubResponses;
-                }
-            }
-
-            public static void AddResponse(ClubResponse clubResponse)
-            {
-                clubResponses.Add(clubResponse);
+                return clubResponses;   
             }
         }
-    }
+        public static void AddResponse(ClubResponse clubResponse)
+        {
+             clubResponses.Add(clubResponse);
+         }
+     }
+ }
 
