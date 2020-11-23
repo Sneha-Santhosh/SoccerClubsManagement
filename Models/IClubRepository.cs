2c5b11c382e797054c1e154d72c56cf521212976
Sneha Santhosh <45783979+Sneha-Santhosh@users.noreply.github.com>
2020-11-22 23:44:47 -0500

    Code cleanup

--- Models/IClubRepository.cs
+++ Models/IClubRepository.cs
@@ -8,7 +8,6 @@ namespace Sneha_S_301096645.Models
     public interface IClubRepository
     {
         IQueryable<ClubResponse> Clubs { get;  }
-       // void AddResponse(ClubResponse clubResponse);
         void SaveClub(ClubResponse club);
         void AddNewClub(ClubResponse club);
         ClubResponse DeleteClub(ClubResponse club);
