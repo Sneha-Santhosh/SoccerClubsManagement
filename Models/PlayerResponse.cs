30fe44beae752e902e86a01665fef43e6022487c
Sneha Santhosh <45783979+Sneha-Santhosh@users.noreply.github.com>
2020-11-22 23:48:47 -0500

    Code cleanup

--- Models/PlayerResponse.cs
+++ Models/PlayerResponse.cs
@@ -12,19 +12,15 @@ namespace Sneha_S_301096645.Models
         public string PlayerName { get; set; }
         [Required(ErrorMessage = "Please enter name of club")]
         public string ClubName { get; set; }
-
         [Required(ErrorMessage = "Please enter ID of club")]
         public int ClubID { get; set; }
         [Key]
         [Required(ErrorMessage = "Please enter ID of player")]
         public int PlayerID { get; set; }
-
         [Required(ErrorMessage = "Please enter  email")]
         [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please provide valid email")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Please choose an option")]
         public bool? Deregister { get; set; }
-
-
     }
 }
